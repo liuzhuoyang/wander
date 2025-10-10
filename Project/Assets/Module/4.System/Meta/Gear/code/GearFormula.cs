56d4d6fb592cf8ac3757a3a8ee72e15396efecc3
Onicore <lu.yuyang@onicore.games>
2025-10-10 18:05:54 +0800

    popup界面和货币资源,以及公式资源

---
+++ Project/Assets/Module/4.System/Meta/Gear/code/GearFormula.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public static class GearFormula
+{
+    public static int GetGearAttack(int baseAttack, int level)
+    {
+        return baseAttack + (int)(baseAttack * 0.1f * level);
+    }
+
+    public static int GetGearNeedCardCount(int level, Rarity rarity)
+    {
+        int baseValue = 20;
+        baseValue = rarity switch
+        {
+            Rarity.Common => 20,
+            Rarity.Rare => 20,
+            Rarity.Epic => 20,
+            Rarity.Legendary => 20,
+            Rarity.Mythic => 20,
+            Rarity.Arcane => 20,
+            _ => 20
+        };
+        return baseValue * (int)Mathf.Pow(2, level - 1);
+    }
+
+    public static int GetGearCoin(int level)
+    {
+        return 100 * level;
+    }
+
+    public static string GetGearCard(string gearName)
+    {
+        return "item_shard_gear_" + gearName;
+    }
+}
