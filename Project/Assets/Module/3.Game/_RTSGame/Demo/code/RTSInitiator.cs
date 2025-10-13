bde12ca13aa41ba0204ebfac01aef2a7a779deb2
kevzn <zeng.zhaopeng@onicore.games>
2025-10-13 16:19:04 +0800

    更新游戏内容配置文件

--- Project/Assets/Module/3.Game/_RTSGame/Demo/code/RTSInitiator.cs
+++ Project/Assets/Module/3.Game/_RTSGame/Demo/code/RTSInitiator.cs
@@ -16,25 +16,13 @@ namespace RTSDemo.Game
     {
         [Header("Init")]
         [SerializeField] private string InitScene;
-        [SerializeField] private GameObject unitManager;
-        [SerializeField] private GameObject buildingManager;
-        [SerializeField] private GameObject bulletManager;
-        [SerializeField] private GameObject gearManager;
-        [SerializeField] private GameObject vfxManager;
+        [SerializeField] private GamePlayConfig gameConfig;
         
         public async void Start()
         {
-            //初始化游戏必要模组
-            //@todo应该有更好的方式来处理模组的初始化
-            var building = Instantiate(buildingManager).GetComponent<BuildingManager>();
-            var unit = Instantiate(unitManager).GetComponent<UnitManager>();
-            var bullet = Instantiate(bulletManager).GetComponent<BulletManager>();
-            var gear = Instantiate(gearManager).GetComponent<GearManager>();
-            var vfx = Instantiate(vfxManager).GetComponent<VFXManager>();
-
-            List<UniTask> loadingTask = new List<UniTask>() { building.Init(), unit.Init(), bullet.Init(), gear.Init(), vfx.Init()};
-            await UniTask.WhenAll(loadingTask);
+            await gameConfig.GamePlaySetUp();
             await SceneManager.LoadSceneAsync(InitScene, LoadSceneMode.Additive);
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName(InitScene));
         }
     }
 }
