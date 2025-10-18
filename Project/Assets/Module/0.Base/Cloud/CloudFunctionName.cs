d3736a54df44b84b74daeabc22449ff174a5c635
Zhuoyang Liu <uoang.liu@gmail.com>
2025-10-18 14:15:04 +0800

    配置DCDN，更新服务里地址

--- Project/Assets/Module/0.Base/Cloud/CloudFunctionName.cs
+++ Project/Assets/Module/0.Base/Cloud/CloudFunctionName.cs
@@ -6,13 +6,13 @@ public class CloudFunctionNames
     // 定义所有函数名称为常量
     //基础
     public const string F_TOKEN = "F_TOKEN";
-    public const string F_GET_TIME = "F_GET_TIME";
+    //public const string F_GET_TIME = "F_GET_TIME";
     public const string F_VERSION = "F_VERSION";
     public const string F_ACCOUNT = "F_ACCOUNT";
     public const string F_SERVER = "F_SERVER";
     public const string F_PROGRESS_SYNC = "F_PROGRESS_SYNC";
     public const string F_ACCESS = "F_ACCESS";
-    public const string F_TEST = "F_TEST";
+    //public const string F_TEST = "F_TEST";
 
     //业务
     public const string F_RANK = "F_RANK";
@@ -20,10 +20,6 @@ public class CloudFunctionNames
     public const string F_GROUP_AB = "F_GROUP_AB";
     public const string F_LEVEL = "F_LEVEL";
 
-    //微信
-    public const string F_WECHAT_GET_OPENID = "F_WECHAT_GET_OPEN_ID";
-    public const string F_WECHAT_IOS_IAP_ORFER = "F_WECHAT_IOS_IAP_OFFER";
-    public const string F_WECHAT_TEXT_INSPECT = "F_WECHAT_TEXT_INSPECT";
     // 其他函数名可以继续定义...
 }
 
@@ -31,16 +27,16 @@ public class CloudFunctionAPI
 {
     private static readonly Dictionary<string, string> functionUrls = new Dictionary<string, string>()
     {
-        { CloudFunctionNames.F_TOKEN, "https://token-dcdn.onicore.host/space_token"},
-        {CloudFunctionNames.F_GET_TIME, "https://space-dcdn.onicore.host/time"},
+        {CloudFunctionNames.F_ACCESS, "https://wander-dcdn.onicore.host/access"},
+        { CloudFunctionNames.F_TOKEN, "https://token-dcdn.onicore.host/wander_token"},
+        //{CloudFunctionNames.F_GET_TIME, "https://space-dcdn.onicore.host/time"},
         {CloudFunctionNames.F_VERSION, "https://space-dcdn.onicore.host/version"},
-        {CloudFunctionNames.F_ACCOUNT, "https://space-dcdn.onicore.host/account"},
-        {CloudFunctionNames.F_PROGRESS_SYNC, "https://space-dcdn.onicore.host/progress"},
-        {CloudFunctionNames.F_MAIL, "https://space-dcdn.onicore.host/mail"},
-        {CloudFunctionNames.F_GROUP_AB, "https://space-dcdn.onicore.host/config_trigger"},
-        {CloudFunctionNames.F_LEVEL, "https://space-dcdn.onicore.host/config_level"},
-        {CloudFunctionNames.F_ACCESS, "https://space-dcdn.onicore.host/access"},
-        {CloudFunctionNames.F_TEST, "https://space-dcdn.onicore.host/test"},
+        {CloudFunctionNames.F_ACCOUNT, "https://wander-dcdn.onicore.host/account"},
+        {CloudFunctionNames.F_PROGRESS_SYNC, "https://wander-dcdn.onicore.host/progress"},
+        {CloudFunctionNames.F_MAIL, "https://wander-dcdn.onicore.host/mail"},
+        {CloudFunctionNames.F_GROUP_AB, "https://wander-dcdn.onicore.host/config_trigger"},
+        {CloudFunctionNames.F_LEVEL, "https://wander-dcdn.onicore.host/config_level"},
+        //{CloudFunctionNames.F_TEST, "https://wander-dcdn.onicore.host/test"},
     };
 
 
